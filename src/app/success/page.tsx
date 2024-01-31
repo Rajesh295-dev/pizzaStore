26172e697944566bd4bdbd26989ccb2e6e49bda8
Rajesh295-dev <{rajeshgautam766@yahoo.com}+{Rajesh295-dev}@users.noreply.github.com>
2024-01-31 16:23:44 -0500

    update again new items ✳️

--- src/app/success/page.tsx
+++ src/app/success/page.tsx
@@ -29,7 +29,7 @@ const SuccessPage = () => {
 
         setTimeout(() => {
           router.push("/orders");
-        }, 2000);
+        }, 1000);
       } catch (err) {
         console.log(err);
       }
